27397d298ced91ddcba4e0fa3dc148ccea78d101
dewangshree <138751486+dewangshree@users.noreply.github.com>
2024-07-12 16:58:39 +0530

    Update table2.sql

--- Ex2/table2.sql
+++ Ex2/table2.sql
@@ -1,58 +1,66 @@
-create table part
-(
-pno number(10),
-pname varchar(20),
-colour varchar(20),
-primary key(pno)
+-- Create part table
+CREATE TABLE part (
+    pno NUMBER(10),
+    pname VARCHAR(20),
+    colour VARCHAR(20),
+    PRIMARY KEY (pno)
 );
 
-Create table supplier
-(
-sno number(10),
-sname varchar(20),
-address varchar(20),
-primary key(sno)
+-- Create supplier table
+CREATE TABLE supplier (
+    sno NUMBER(10),
+    sname VARCHAR(20),
+    address VARCHAR(20),
+    PRIMARY KEY (sno)
 );
 
-create table supply
-(
-pno number(10),
-sno number(10),
-quantity varchar(20),
-primary key(pno,sno),
-foreign key(pno) references part(pno)on delete cascade,
-foreign key(sno) references supplier(sno)on delete cascade
+-- Create supply table
+CREATE TABLE supply (
+    pno NUMBER(10),
+    sno NUMBER(10),
+    quantity VARCHAR(20),
+    PRIMARY KEY (pno, sno),
+    FOREIGN KEY (pno) REFERENCES part(pno) ON DELETE CASCADE,
+    FOREIGN KEY (sno) REFERENCES supplier(sno) ON DELETE CASCADE
 );
 
-insert into part values(1,'plug','black');
-insert into part values(2,'bolt','blue');
-insert into part values(3,'nut','green');
-insert into supplier values(10,'Anoop','udupi');
-insert into supplier values(15,'Bharath','mangalore');
-insert into supplier values(20,'Ram','bangalore');
-insert into supply values(1,10,50);
-insert into supply values(2,10,30);
-insert into supply values(1,15,70);
-insert into supply values(3,15,40);
-insert into supply values(1,20,55);
-insert into supply values(2,20,65);
-insert into supply values(3,20,75);
+-- Insert data into part table
+INSERT INTO part VALUES (1, 'plug', 'black');
+INSERT INTO part VALUES (2, 'bolt', 'blue');
+INSERT INTO part VALUES (3, 'nut', 'green');
 
-select * from part;
+-- Insert data into supplier table
+INSERT INTO supplier VALUES (10, 'Anoop', 'udupi');
+INSERT INTO supplier VALUES (15, 'Bharath', 'mangalore');
+INSERT INTO supplier VALUES (20, 'Ram', 'bangalore');
 
-select * from supply ;
+-- Insert data into supply table
+INSERT INTO supply VALUES (1, 10, '50');
+INSERT INTO supply VALUES (2, 10, '30');
+INSERT INTO supply VALUES (1, 15, '70');
+INSERT INTO supply VALUES (3, 15, '40');
+INSERT INTO supply VALUES (1, 20, '55');
+INSERT INTO supply VALUES (2, 20, '65');
+INSERT INTO supply VALUES (3, 20, '75');
 
-select* from supplier ;
+-- Select data from part table
+SELECT * FROM part;
 
+-- Select data from supply table
+SELECT * FROM supply;
 
-​select pno  from supply
-​where sno IN(select sno from  
-             ​​supplier where sname='Ram');
- 
+-- Select data from supplier table
+SELECT * FROM supplier;
 
-​select sname,pname
-​from supplier,supply,part  
-​where pname='bolt' AND supply.sno=supplier.sno AND part.pno=supply.pno; 
-​
+-- Select pno from supply where sno is supplier 'Ram'
+SELECT pno 
+FROM supply
+WHERE sno IN (SELECT sno FROM supplier WHERE sname = 'Ram');
 
-​delete from part where colour='green';
+-- Select sname and pname where pname is 'bolt'
+SELECT sname, pname
+FROM supplier s, supply sp, part p
+WHERE p.pname = 'bolt' AND sp.sno = s.sno AND sp.pno = p.pno;
+
+-- Delete rows from part table where colour is 'green'
+DELETE FROM part WHERE colour = 'green';
